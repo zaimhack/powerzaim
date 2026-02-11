e3b7983fee5d33b7e80f685163cf104d3cf78349
ynxdaz <ynxdaz@users.noreply.github.com>
2026-02-12 02:05:17 +0900

    Add a rule to detect plus-minus balance transactions

--- Test-PowzTnxsInconsistency.ps1
+++ Test-PowzTnxsInconsistency.ps1
@@ -10,12 +10,12 @@ begin {
     function Test-SendAndRecv {
         param ($tnx1, $tnx2)
     
-                    if (($buffer[$i].Account -eq $buffer[$j].Account) -and `
-                        $buffer[$i].Type -eq "balance" -and $buffer[$j].Type -eq "balance" -and `
-                        [Math]::Abs($buffer[$i].Amount) -eq [Math]::Abs($buffer[$j].Amount) -and `
-                        ([int]$buffer[$i].Amount * [int]$buffer[$j].Amount) -lt 0) {
-                        $detectedPairs[$j] = $i
-                    }
+        if (($buffer[$i].Account -eq $buffer[$j].Account) -and `
+                $buffer[$i].Type -eq "balance" -and $buffer[$j].Type -eq "balance" -and `
+                [Math]::Abs($buffer[$i].Amount) -eq [Math]::Abs($buffer[$j].Amount) -and `
+            ([int]$buffer[$i].Amount * [int]$buffer[$j].Amount) -lt 0) {
+            $detectedPairs[$j] = $i
+        }
     }
 
     function Test-A {
@@ -30,35 +30,35 @@ begin {
 
     function Test-C {
         param ($tnx1, $tnx2)
-        return ([Math]::Abs($tnx1.支出) -eq [Math]::Abs($tnx2.残高調整))
+        return ([Math]::Abs($tnx1.残高調整) -eq [Math]::Abs($tnx2.残高調整) -and $tnx1.方法 -eq "balance" -and $tnx2.方法 -eq "balance" -and ($tnx1.支払元 -eq $tnx2.入金先 -or $tnx1.入金先 -eq $tnx2.支払元))
     }
 }
 
 process {
-    $p | ForEach-Object {
-        if ($null -eq $lastDate) {
-            $lastDate = $p.日付
-        }
+    if ($null -eq $lastDate) {
+        $lastDate = $p.日付
+    }
 
-        if ($lastDate -eq $_.日付) {
-            $tnxs.Add($_) | Out-Null
-        } else {
-            $detectedPairs = New-Object -Type System.Collections.ArrayList
-            for ($i = 0; $i -lt $tnxs.Count - 1; ++$i) {
-                for ($j = 1; $j -lt $tnxs.Count; ++$j) {
+    if ($lastDate -eq $p.日付) {
+        $tnxs.Add($p) | Out-Null
+    } else {
+        $detectedPairs = New-Object -Type System.Collections.ArrayList
+        for ($i = 0; $i -lt $tnxs.Count - 1; ++$i) {
+            for ($j = 1; $j -lt $tnxs.Count; ++$j) {
 
-                    if (Test-A $tnxs[$i] $tnxs[$j]) {
-                        $detectedPairs.Add(@($i, $j)) | Out-Null
-                    }
+                if (Test-A $tnxs[$i] $tnxs[$j]) {
+                    $detectedPairs.Add(@("A", $i, $j)) | Out-Null
+                } elseif (Test-C $tnxs[$i] $tnxs[$j]) {
+                    $detectedPairs.Add(@("C", $i, $j)) | Out-Null
                 }
             }
-            $detectedPairs |% {
-                , @($tnxs[$_[0]], $tnxs[$_[1]])
-            }
-            $tnxs.Clear()
         }
-        $lastDate = $_.日付
+        $detectedPairs | % {
+            [System.ValueTuple]::Create[string, object, object]($_[0], $tnxs[$_[1]], $tnxs[$_[2]])
+        }
+        $tnxs.Clear()
     }
+    $lastDate = $_.日付
 }
 
 end {
